1b75052916b9a64033a4fde56855ea2279716a52
kartik-dvst <kartik.yash11@gmail.com>
2026-04-12 09:24:50 +0530

    set the bottom padding to 0 inside MainActivity.kt

--- app/src/main/java/com/example/wanderlens/MainActivity.kt
+++ app/src/main/java/com/example/wanderlens/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            // CHANGES: I've set the bottom padding to 0 instead of systemBars.bottom
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
